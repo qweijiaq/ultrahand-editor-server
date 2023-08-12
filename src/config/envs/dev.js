c0c4cfd1f336b05bf24923afc7b9a8d8c3196d49
WeiJia <2112025700@qq.com>
2023-08-12 18:13:41 +0800

    初始化 MongoDB 和 Redis 数据库

--- src/config/envs/dev.js
+++ src/config/envs/dev.js
@@ -12,4 +12,17 @@ module.exports = {
     port: "3306",
     database: "ultrahand",
   },
+
+  // mongodb 连接配置
+  mongodbConf: {
+    host: "localhost",
+    port: "27017",
+    dbName: "ultrahand",
+  },
+
+  // redis 连接配置
+  redisConf: {
+    port: "6379",
+    host: "127.0.0.1",
+  },
 };
