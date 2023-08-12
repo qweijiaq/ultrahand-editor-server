7dbf1c1a5af031d0708e88663cf4ce1f557d6104
WeiJia <2112025700@qq.com>
2023-08-12 17:10:20 +0800

    初始化 MySQL 数据库

---
+++ src/config/envs/dev.js
@@ -0,0 +1,15 @@
+/**
+ * @description dev 配置
+ * @author Jia Wei
+ */
+
+module.exports = {
+  // mysql 连接配置
+  mysqlConf: {
+    host: "localhost",
+    user: "root",
+    password: "weijia123",
+    port: "3306",
+    database: "ultrahand",
+  },
+};
---
+++ src/db/seq/utils/conn-test.js
@@ -0,0 +1,19 @@
+/**
+ * @description 数据量连接测试
+ * @author Jia Wei
+ */
+
+const seq = require("../seq");
+
+// 测试连接，直接运行 node src/db/seq/utils/conn-test.js
+seq
+  .authenticate()
+  .then(() => {
+    console.log("ok");
+  })
+  .catch(() => {
+    console.log("fail");
+  })
+  .finally(() => {
+    process.exit();
+  });
